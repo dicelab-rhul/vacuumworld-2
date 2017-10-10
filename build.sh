1e2a728dbf8bccc0d567f8d0af2a81a1d3469acf
cloudstrife9999 <Emanuele.Uliana.2016@live.rhul.ac.uk>
2017-10-10 16:35:38 +0100

    Minor.

--- build.sh
+++ build.sh
@@ -9,6 +9,15 @@ if [ "" == "$PKG_OK" ]; then
   exit -1
 fi
 
+PKG="python3"
+PKG_OK=$(which "$PKG")
+echo Checking for "$PKG"... $PKG_OK
+
+if [ "" == "$PKG_OK" ]; then
+  echo "$PKG is not installed. Aborting. Install $PKG and retry."
+  exit -1
+fi
+
 JAR_NAME="vw.jar"
 echo "Generating ANT script..."
 ./ant_generator.py -H $HOME -j $JAR_NAME
@@ -16,4 +25,5 @@ echo "Done"
 
 echo "Generating $HOME/$JAR_NAME"
 ant -f ant.xml
+echo "Succesfully created $HOME/$JAR_NAME"
 echo "Done."
